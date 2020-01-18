a278946e4068b184c9cb3dfce4d7128528f90172
EternalFlyn <EternalFlyn@gmail.com>
2020-01-19 01:54:39 +0800

    1.0.1a
    2020/01/19

---
+++ src/main/java/com/flyn/neural_networks/App.java
@@ -0,0 +1,13 @@
+package com.flyn.neural_networks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
